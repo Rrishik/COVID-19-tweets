3fc106eca7b072d3d9fcd6318e499a03c3c48fe0
rishik <ramenarshik@gmail.com>
2020-05-18 05:45:01 +0530

    added yesterday's tweets

--- get_old_tweets.py
+++ get_old_tweets.py
@@ -6,7 +6,6 @@ import tweepy
 # !pip install python-twitter
 import twitter
 import csv
-# from kaggle_secrets import UserSecretsClient
 from os import path
 import config
 
@@ -16,65 +15,52 @@ CONSUMER_KEY = config.CONSUMER_KEY
 CONSUMER_SECRET = config.CONSUMER_SECRET
 
 
-def connect_to_twitter_Auth():
+def connectAuth():
     auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
 
-def extract_tweets(tweet_object):
+def extractIds(tweet_object):
     tweet_list =[]
     for tweet in tweet_object:
         id = tweet.id
-        if tweet.place is not None:
-            country = tweet.place.country
-        else:
-            country = None
-        text = tweet.full_text
-        created_at = tweet.created_at
-        tweet_list.append({'id':id, 'created_at':created_at,'text':text, 'country':country})
-    df = pd.DataFrame(tweet_list, columns=['id','created_at','text', 'country'])
+        tweet_list.append({'id':id})
+    df = pd.DataFrame(tweet_list, columns=['id'])
     return df
 
 hashtags = ["#coronavirus", "#coronavirusoutbreak", "#coronavirusPandemic", "#covid19", "#covid_19", "#epitwitter", "#ihavecorona"]
-searchQuery = (" OR ").join(hashtags) + " -filter:retweets"
+query = (" OR ").join(hashtags) + " -filter:retweets"
 
-api = connect_to_twitter_Auth()
-if (not api):
-    print ("Can't Authenticate :(")
-    sys.exit(-1)
 
-tweetsPerQry = 100
 sinceId = None
-max_id = 1256735220293791744
+max_id = 1258641165982806019
 
-
+api = connectAuth()
 while(1):
     try:
         if (max_id <= 0):
             if (not sinceId):
-                new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode="extended", lang = 'en')
+                new_tweets = api.search(q = query, count = 100, lang = 'en')
             else:
-                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                        since_id=sinceId, tweet_mode="extended", lang = 'en')
+                new_tweets = api.search(q = query, count = 100, since_id = sinceId, lang = 'en')
         else:
             if (not sinceId):
-                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                        max_id=str(max_id - 1), tweet_mode="extended", lang = 'en')
+                new_tweets = api.search(q = query, count = 100, max_id = str(max_id - 1), lang = 'en')
             else:
-                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                        max_id=str(max_id - 1),
-                                        since_id=sinceId, tweet_mode="extended", lang = 'en')
+                new_tweets = api.search(q = query, count = 100, max_id = str(max_id - 1), since_id = sinceId, lang = 'en')
+        
         if not new_tweets:
             print("No more tweets found")
             break
-        tweets_df = extract_tweets(new_tweets)
+        tweets_df = extractIds(new_tweets)
+        
         if(path.exists('old_tweets.csv')):
             tweets_df.to_csv('old_tweets.csv', mode='a', index=False, header=None)
         else:
             tweets_df.to_csv('old_tweets.csv', mode='a', index=False)
-#        print(tweets_df.iloc[-5:].id)
-#        print()
+
         max_id = tweets_df.iloc[-1].id
+        
     except tweepy.TweepError as e:
         print("some error : " + str(e))
         break
--- get_tweets.py
+++ get_tweets.py
@@ -16,39 +16,40 @@ CONSUMER_KEY = config.CONSUMER_KEY
 CONSUMER_SECRET = config.CONSUMER_SECRET
 
 
-def connect_to_twitter_OAuth():
+def connectOAuth():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
     return api
 
-def extract_tweets(tweet_object):
+def extractIds(tweet_object):
     tweet_list =[]
     for tweet in tweet_object:
         id = tweet.id_str
-        if tweet.place is not None:
-            country = tweet.place.country
-        else:
-            country = None
-        text = tweet.full_text
-        created_at = tweet.created_at
-        tweet_list.append({'id':id, 'created_at':created_at,'text':text, 'country':country})
-    df = pd.DataFrame(tweet_list, columns=['id','created_at','text', 'country'])
+        tweet_list.append({'id':id})
+    df = pd.DataFrame(tweet_list, columns=['id'])
     return df
 
+# List of hashtags to track
 hashtags = ["#coronavirus", "#coronavirusoutbreak", "#coronavirusPandemic", "#covid19", "#covid_19", "#epitwitter", "#ihavecorona"]
+
+# Building the search query
 query = (" OR ").join(hashtags) + " -filter:retweets"
 
+id_high = 1261836344239755264 # Seed id to start searching for tweets after this id(useful for the case when you have to resume getting tweets from a specific id
 
-api = connect_to_twitter_OAuth()
-id_high = 1258576859773583360
+api = connectOAuth()
+# This script runs 24*7 without using much of the system resources
+
 while(1):
     
-    search_results = api.search(q=query, count=200, lang='en', tweet_mode="extended", since_id = str(id_high))
-    tweets_df = extract_tweets(search_results)
+    search_results = api.search(q = query, count = 100, lang = 'en', since_id = str(id_high))
+    tweets_df = extractIds(search_results)
+    
     if(path.exists('tweets.csv')):
-        tweets_df.to_csv('tweets.csv', mode='a', index=False, header=None)
+        tweets_df.to_csv('tweets.csv', mode = 'a', index=False, header=None)
     else:
-        tweets_df.to_csv('tweets.csv', mode='a', index=False)
-    id_high = max(tweets_df['id'])
+        tweets_df.to_csv('tweets.csv', mode = 'a', index=False)
+    if(not tweets_df.empty):
+        id_high = max(tweets_df['id']) # Updating the seed id from which to start searching in the next iteration 
     time.sleep(12)
