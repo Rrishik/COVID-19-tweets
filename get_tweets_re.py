1cf75ea9971747faefee65de8e733bb9102eb834
rishik <ramenarshik@gmail.com>
2020-06-04 11:51:10 +0530

    Site updated

--- get_tweets_re.py
+++ get_tweets_re.py
@@ -41,7 +41,7 @@ hashtags = ["#coronavirus", "#coronavirusoutbreak", "#coronavirusPandemic", "#co
 query = (" OR ").join(hashtags)
 
 # 99999999999999999999
-max_id = 1267969244127301632
+max_id = 1268331632123162624
 
 
 # This script was scheduled to run daily, so the filenames to be processed was yesterday's date
