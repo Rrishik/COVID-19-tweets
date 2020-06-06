9b6519474c6aa1a3ef95612d91cc1271c17ae19c
rishik <ramenarshik@gmail.com>
2020-06-06 09:40:02 +0530

    added yesterday's tweets

--- get_tweets_re.py
+++ get_tweets_re.py
@@ -41,7 +41,7 @@ hashtags = ["#coronavirus", "#coronavirusoutbreak", "#coronavirusPandemic", "#co
 query = (" OR ").join(hashtags)
 
 # 99999999999999999999
-max_id = 1268331632123162624
+max_id = 1268694020861108224
 
 
 # This script was scheduled to run daily, so the filenames to be processed was yesterday's date
